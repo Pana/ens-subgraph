728e9726c639ac92db972ae21eb7eb132bc0bc0c
PanaW <pana.wang@outlook.com>
2022-04-22 11:02:14 +0800

    reverse fake label logic

--- src/ethRegistrar.ts
+++ src/ethRegistrar.ts
@@ -41,8 +41,8 @@ export function handleNameRegistered(event: NameRegisteredEvent): void {
   registration.expiryDate = event.params.expires
   registration.registrant = account.id
 
-  // let labelName = ens.nameByHash(label.toHexString())
-  let labelName: string | null = null
+  let labelName = ens.nameByHash(label.toHexString())
+  // let labelName: string | null = null
   if (labelName != null) {
     registration.labelName = labelName
   }
